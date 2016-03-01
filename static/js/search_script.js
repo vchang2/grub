833c6e54e3f6be9b7417cafefb29513881af3286
Adam Young <adamy@stanford.edu>
2016-03-01 02:46:14 -0800

    progress on photo area

--- static/js/search_script.js
+++ static/js/search_script.js
@@ -1,10 +1,14 @@
 function SearchScript(displayID){
-	var IMG_HEIGHT = 200;
-	var IMG_WIDTH = 200;
-	var padding = 10
+	var IMG_HEIGHT = 300;
+	var IMG_WIDTH = 400;
+	var padding = 20;
 
 	displayDiv = document.getElementById(displayID);
 	var numPhotos = document.getElementById("numPhotos").value;
+	if(numPhotos == 0){
+		return;
+	}
+
 	var displayDivStyle = window.getComputedStyle(displayDiv);
 	var width = displayDivStyle.width;
 	//var left = displayDivStyle.offsetLeft;
@@ -15,24 +19,32 @@ function SearchScript(displayID){
 	var offset = findPos(displayDiv);
 	//window.alert(offset['X']);
 
+	var numLines = 1;
 
 	for(var i = 0; i < numPhotos; i++){
-		var image = document.createElement("img");
-		image.src = document.getElementById("photo" + i).value;
+		var image = document.getElementById("recipePhoto" + i);
+		if(image == null){
+			image = document.createElement("img");
+			image.id = "recipePhoto" + i;
+			image.src = document.getElementById("photo" + i).value;
 
 
-		image.style.height = IMG_HEIGHT + "px";
-		image.style.position = "fixed";
-		//image.style.clip = "rect(0px 75px 75px 0px)";
-		displayDiv.appendChild(image);
-		var width = image.clientWidth;
+			image.style.height = IMG_HEIGHT + "px";
+			image.style.position = "fixed";
+			//image.style.clip = "rect(0px 75px 75px 0px)";
+			displayDiv.appendChild(image);
+			var width = image.clientWidth;
 
-		if (image.clientWidth > IMG_WIDTH){
-			//window.alert(image.clientWidth);
-			var widthToRemove = image.clientWidth - IMG_WIDTH;
-			image.style.clip = "rect(0px " + IMG_WIDTH + "px " + IMG_HEIGHT +  "px 0px)"
-			//image.style.clip = "0px 0px 0px 0px"
+			if (image.clientWidth > IMG_WIDTH){
+				//window.alert(image.clientWidth);
+				var widthToRemove = image.clientWidth - IMG_WIDTH;
+				//image.style.clip = "rect(0px " + IMG_WIDTH + "px " + IMG_HEIGHT +  "px 0px)";
+				image.style.clip = "rect(0px " + IMG_WIDTH + "px " + IMG_HEIGHT +  "px 0px)";
+				//image.style.clip = "0px 0px 0px 0px"
+			}
 		}
+		
+		var width = image.clientWidth;
 
 		deltaX = width;
 		if (width > IMG_WIDTH){
@@ -44,6 +56,7 @@ function SearchScript(displayID){
 		if (offset['X'] + deltaX > window.innerWidth - originalOffset['X'] - padding){
 			offset['X'] = originalOffset['X'];
 			offset['Y'] += IMG_HEIGHT + padding;
+			numLines += 1;
 		}
 
 		image.style.left = "" + offset['X'] + "px";
@@ -51,10 +64,12 @@ function SearchScript(displayID){
 		offset['X'] += deltaX;
 
 
-
-
 		//image.style.boxShadow = "10px 20px 30px blue inset";
 		//displayDiv.appendChild(image);
+	}
+
+	for(var i = 0; i < numLines; i++){
+		displayDiv.innerHTML += '<div style=\"font-size:' + (IMG_HEIGHT + padding) + 'px\"><br/></div>';
 	}
 }
 
